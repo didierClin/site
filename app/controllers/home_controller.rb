a86fb3098013f194830f4ad4b489c1602f217a5b
didierClin <didier.c62@numericable.fr>
2017-02-27 20:25:48 +0100

    add controller and force login to private pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  before_action :authenticate_user!, only: :private
+  def index
+  end
+
+  def private
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/private'
+
   devise_for :users
+
+  root 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
